294f1935eed9d16fb2b246272e714078f4e7d6ca
sajib689 <sajibbabu751@gmail.com>
2025-05-05 00:05:00 +0600

    first commit

---
+++ output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const country = "Kodu";
+const toDo = (a, b) => {
+    return a + b;
+};
+toDo(1, 2);
+console.log(country);
---
+++ src/script.ts
@@ -0,0 +1,11 @@
+
+const country = "Kodu";
+
+const toDo = (a: number, b: number) => {
+    return a + b;
+}
+
+
+toDo(1, 2)
+
+console.log(country)
