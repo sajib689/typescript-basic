78a8ad711cb85c155e3a357d60265bafc9c695bd
sajib689 <sajibbabu751@gmail.com>
2025-05-06 23:13:06 +0600

    add functionSign.js and functionSign.ts to implement addition functionality

--- output/script.js
+++ output/script.js
@@ -14,3 +14,13 @@ person.name = "Jane";
 person.age = 25;
 person.country = "Canada";
 console.log(person);
+const myFunc = (a, b) => {
+    console.log(`Hello ${a} ${b}`);
+    return a + b;
+};
+myFunc("John", "Doe");
+const manFunction = (person) => {
+    console.log(`Hello ${person.name} ${person.age} ${person.country}`);
+    return person.name + person.age + person.country;
+};
+manFunction({ name: "John", age: 30, country: "USA" });
